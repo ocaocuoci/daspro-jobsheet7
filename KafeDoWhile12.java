69415b2a43622b12048abf642d4ed1431db9413c
ocaocuoci <hernandar0606@gmail.com>
2024-10-10 06:22:03 +0700

    p3

---
+++ KafeDoWhile12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class KafeDoWhile12{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        int kopi, teh, roti;
+        String namaPelanggan;
+        double hargaTeh = 7000, hargaKopi = 12000, hargaRoti = 20000;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")){
+                System.out.println("Transaksi dibatalkan");
+                break;
+            }
+            System.out.print("Jumlah Kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah Teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah Roti: ");
+            roti = sc.nextInt();
+
+            double totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total yang harus dibayar: Rp" +  totalHarga);
+            sc.nextLine();
+        } while (true);
+        System.out.println("Ssemua transaksi selesai.");
+        
+    }
+}
