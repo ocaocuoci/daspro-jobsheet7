2608cb4291852d439be9cd7c8e0bf2a346c734f3
ocaocuoci <hernandar0606@gmail.com>
2024-10-10 08:14:53 +0700

    bismillah terakhir

--- TempatParkir12.java
+++ TempatParkir12.java
@@ -9,25 +9,27 @@ public class TempatParkir12{
             System.out.println("Jenis Kendaraan");
             System.out.println("1. Mobil");
             System.out.println("2. Motor");
-            System.out.println("0. (keluar )");
+            System.out.println("0. (keluar)");
             System.out.print("Masukkan angka dari jenis kendaraan yang dipilih: ");
             jeniskendaraan = sc12.nextInt();
-            System.out.print("Masukkan durasi parkir: ");
+            System.out.print("Masukkan durasi parkir (jam): ");
             durasi = sc12.nextInt();
             
             if (jeniskendaraan == 1 && jeniskendaraan == 2){
-                System.out.print("Durasi parkir: " + durasi);
-            } if (durasi >5){
+                  if (durasi >5){
                 total += 12500;
                 System.out.println("Total: " + total);
-            } else if (jeniskendaraan == 1){
+                } else if (jeniskendaraan == 1){
                 total += durasi * 3000;
                 System.out.println("Total: " + total);
-            } else if (jeniskendaraan == 2){
+                } else if (jeniskendaraan == 2){
                 total += durasi * 2000;
                 System.out.println("Total: " + total);
+                }
+                 System.out.println("Total keseluruhan: " + total);
             }
+
+           
         } while (jeniskendaraan !=0);
-        System.out.println("Total keseluruhan: " + total);
     }
 }
