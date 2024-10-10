6ac658c3428a4ed553e863abf6de843aaf8eae9b
ocaocuoci <hernandar0606@gmail.com>
2024-10-10 07:11:11 +0700

    tugas bioskop revisi lagi

--- TugasBioskop12.java
+++ TugasBioskop12.java
@@ -3,28 +3,42 @@ public class TugasBioskop12{
     public static void main(String[] args) {
         Scanner input12 =new Scanner(System.in);
 
-        double totalTiket =0, totalHarga = 0, diskon;
+        double totalTiket = 0, totalHarga = 0, diskon, totalPenjualan = 0.0;
         int tiket = 0, pelanggan, i = 1;
         
-        System.out.println("--- REKAP TOTAL PENJUALAN TIKET BIOSKOP ---");
+        System.out.println("--- REKAP TOTAL PENJUALAN TIKET BIOSKOP ---/n");
         System.out.println("Masukkan jumlah pelanggan dalam sehari: ");
         pelanggan = input12.nextInt();
-        while (i < pelanggan) {
+
+        while (i <= pelanggan) {
             System.out.print("Masukkan jumlah pembelian tiket pelanggan ke-" + i + ": ");
-            tiket = input12.nextInt();
-            if (tiket > 4) {
-                 diskon = 0.9;
-            } else if (tiket > 10) {
-                 diskon = 0.85;
-            } else {
-                System.out.println("Tiket tidak valid");
+            tiket = input12.nextInt(); 
+
+            if (tiket < 0){
+                System.out.print("Tidak valid. Masukkan jumlah tiket dengan benar");
                 continue;
             }
+            totalHarga = tiket * 50000;
+
+            if (tiket > 4 && tiket <= 10){
+                diskon = (1.0 - 0.1);
+                totalHarga *= diskon;
+            } else if (tiket > 10){
+                diskon = (1.0 - 0.15);
+                totalHarga *= diskon;
+            } else {
+                diskon = (1.0 - 0.0);
+                totalHarga *= diskon;
+            }
+            i++;
+            System.out.println("Harga tiket yang dibayar: " + totalHarga);
             totalTiket += tiket;
-            System.out.println("Jumlah tiket: " + totalTiket);
-            totalHarga = totalTiket * 50000 * diskon;
-            System.out.println("Total pemasukan: " + totalHarga);
-        i++;
-        } 
+            totalPenjualan += totalHarga;
+
+        }  
+        System.out.println("--- REKAP TOTAL PENJUALAN TIKET BIOSKOP ---/n");
+        System.out.println("Jumlah tiket (dalam sehari): " + totalTiket);
+        System.out.println("Total pemasukan (dalam sehaari): " + totalPenjualan);
+        System.out.println("---------------------------------------------");
     }
 }
